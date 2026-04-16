ba3fb88f111c4c4b3c3cfd6946325eba56e753b6
smilefinanceAI <aarifkhankhatri00786@gmail.com>
2026-04-16 13:27:09 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  generateEtags: true,
-  compiler: {
-    removeConsole: process.env.NODE_ENV === 'production',
-  },
 }
 
 module.exports = nextConfig
